c654897b6cb513491e8432a86f17ba0e2f3a68c4
Zerickelogy <zerickpoon@gmail.com>
2018-08-11 17:38:20 +0800

    Additions Part 2

---
+++ footer.php
@@ -0,0 +1,40 @@
+<div class="container footer">
+    <div class="footer-item sitemap">
+        <h5>Sitemap</h5>
+        <div class="footer-subitem">
+            <a href="index.html">Home</a><br/>
+            <a href="#">Merchants</a><br/>
+            <a href="promotions.html">Promotions</a><br/>
+            <a href="faq.html">FAQs</a><br/>
+        </div>
+    </div>
+    <div class="footer-item my-account">
+        <h5>My Account</h5>
+        <div class="footer-subitem">
+            <a href="#">Edit Your Details</a><br/>
+            <a href="#">Forget Password</a><br/>
+        </div>
+    </div>
+    <div class="footer-item lucxury">
+        <h5>Lucxury</h5>
+        <div class="footer-subitem">
+            <a href="#">Terms & Conditions</a><br/>
+            <a href="#">Privacy Policy</a><br/>
+            <a href="#">Contact Us</a><br/>
+        </div>
+    </div>
+    <div class="footer-item find-us">
+        <h5>Find Us</h5>
+        <div class="footer-subitem">
+            <a href="https://www.facebook.com/lucxuryglobal" target="_blank"><i class="fab fa-facebook-square"></i></a>
+            <a href="https://www.instagram.com/lucxuryglobal/" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="https://twitter.com/lucxuryglobal" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.youtube.com/channel/UCeGiwGkYZ9p8kxj8YwN4fIw?view_as=subscriber" target="_blank"><i class="fab fa-youtube"></i></a>
+        </div>
+    </div>
+</div>
+
+<div class="container footer-credits">
+    <p id="credits-left">© 2018 Lucxury</p>
+    <p id="credits-right">All Rights Reserved.</p>
+</div>
