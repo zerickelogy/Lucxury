8dad44cb0126e96124396c0879127868fd978a08
glenn <glenn@192.168.1.15>
2018-08-25 02:49:26 +0800

    Merge origin/master
    Conflicts:
    	index.php
    	login.php

--- Webservices/getImageUrlFromItemFilterId.php
+++ Webservices/getImageUrlFromItemFilterId.php
@@ -1,10 +1,9 @@
 <?php
-$itemstorage_id = $_GET["itemstorage_id"];
+$item_storage_id = $_GET["itemstorage_id"];
 include 'dbconn.php';
-$query = "SELECT item_storage.itemstorage_id, image_storage.itemstorage_image_url 
-FROM item_storage 
-INNER JOIN image_storage on item_storage.itemstorage_id = image_storage.itemstorage_id
-WHERE item_storage.itemstorage_id = '$itemstorage_id'";
+$query = "SELECT * FROM item_storage 
+INNER JOIN image_storage on item_storage.item_storage_id = image_storage.item_storage_id
+WHERE item_storage.item_storage_id = '$item_storage_id'";
 $result = mysqli_query($link, $query);
 while ($row = mysqli_fetch_assoc($result)) {
     $students[] = $row;
