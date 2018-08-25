a160ec0ae0c1e3e7ea4a5bf737afe7c9433c7480
Zerickelogy <zerickpoon@gmail.com>
2018-08-25 12:09:15 +0800

    footer index

--- index.php
+++ index.php
@@ -127,37 +127,7 @@ session_start();
 
         </div>
 
-        <div class="container footer">
-            <div class="footer-item my-account">
-                <h5>My Account</h5>
-                <div class="footer-subitem">
-                    <a href="edit-profile.html">Edit Your Details</a><br/>
-                    <a href="#">Forget Password</a><br/>
-                </div>
-            </div>
-            <div class="footer-item lucxury">
-                <h5>Lucxury</h5>
-                <div class="footer-subitem">
-                    <a href="#">Terms of Use</a><br/>
-                    <a href="#">Privacy Policy</a><br/>
-                    <a href="#">Contact Us</a><br/>
-                </div>
-            </div>
-            <div class="footer-item find-us">
-                <h5>Find Us</h5>
-                <div class="footer-subitem">
-                    <a href="https://www.facebook.com/lucxuryglobal" target="_blank"><i class="fab fa-facebook-square"></i></a>
-                    <a href="https://www.instagram.com/lucxuryglobal/" target="_blank"><i class="fab fa-instagram"></i></a>
-                    <a href="https://twitter.com/lucxuryglobal" target="_blank"><i class="fab fa-twitter"></i></a>
-                    <a href="https://www.youtube.com/channel/UCeGiwGkYZ9p8kxj8YwN4fIw?view_as=subscriber" target="_blank"><i class="fab fa-youtube"></i></a>
-                </div>
-            </div>
-        </div>
-
-        <div class="container footer-credits">
-            <p id="credits-left">© 2018 Lucxury</p>
-            <p id="credits-right">All Rights Reserved.</p>
-        </div>
+        <?php include 'footer.php'; ?>
 
 
     </main>
