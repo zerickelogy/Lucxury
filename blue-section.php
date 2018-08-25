6e79ca0136b8ce7b79b8bef3587f2abcb95d5d79
Zerickelogy <zerickpoon@gmail.com>
2018-08-25 17:34:41 +0800

    Blue Section

--- blue-section.php
+++ blue-section.php
@@ -1,6 +1,22 @@
-<?php
 
-?>
+<div class="sidebar">
+    <div class="sidebar-item user-id"><i class="fas fa-user"></i><a href="#">Hello, <?php echo $_SESSION["username"]; ?></a></div>
+
+    <!--<div class="sidebar-item category cat-selector"><a href="#">Men</a><a href="#">Women</a></div>-->
+
+    <ul class="sidebar-list">
+        <li class="sidebar-item"><a href="https://lucxury.com/lucxury_wp/" class="sidebar-anchor">News / Media</a></li>
+        <li class="sidebar-item"><a href="promotions.php" class="sidebar-anchor">Promotions</a></li>
+        <li class="sidebar-item"><a href="faq.php" class="sidebar-anchor">FAQs</a></li>
+        <li class="sidebar-item"><a href="edit-profile.php" class="sidebar-anchor">Edit profile</a></li>
+    </ul>
+
+
+    <div onclick="logout()" class="sidebar-item logout-btn"><a onclick="logout()" href="#">Logout</a></div>
+
+</div>
+
+
 <script>
     $(document).ready(function () {
         console.log('<?php // echo $_SESSION['username'];   ?>');
@@ -24,19 +40,3 @@
     }
 
 </script>
-<div class="sidebar">
-    <div class="sidebar-item user-id"><i class="fas fa-user"></i><a href="#">Hello, <?php echo $_SESSION["username"]; ?></a></div>
-
-    <!--<div class="sidebar-item category cat-selector"><a href="#">Men</a><a href="#">Women</a></div>-->
-
-    <ul class="sidebar-list">
-        <li class="sidebar-item"><a href="https://lucxury.com/lucxury_wp/" class="sidebar-anchor">News / Media</a></li>
-        <li class="sidebar-item"><a href="promotions.php" class="sidebar-anchor">Promotions</a></li>
-        <li class="sidebar-item"><a href="faq.php" class="sidebar-anchor">FAQs</a></li>
-        <li class="sidebar-item"><a href="edit-profile.php" class="sidebar-anchor">Edit profile</a></li>
-    </ul>
-
-
-    <div onclick="logout()" class="sidebar-item logout-btn"><a onclick="logout()" href="#">Logout</a></div>
-
-</div>
