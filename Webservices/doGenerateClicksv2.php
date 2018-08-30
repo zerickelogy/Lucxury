f9f3cad2d6707d90e83289b32ea99339b1015d04
glenn <glenn@192.168.1.15>
2018-08-31 00:40:51 +0800

    Merge origin/master
    Conflicts:
    	index.php
    	login.php

---
+++ Webservices/doGenerateClicksv2.php
@@ -0,0 +1,14 @@
+<?php
+
+include './dbconn.php';
+session_start();
+$item_id = $_GET["item_id"];
+$user_id = $_SESSION["user_id"];
+
+$queryTimeAndDate = "SELECT NOW() as `now`";
+$result6 = mysqli_query($link, $queryTimeAndDate);
+$row = $result6->fetch_array();
+$now = $row['now'];
+
+// insert into user table
+$query2 = "INSERT INTO `click_storage` (`click_id`, `user_id`, `item_storage_id`, `datetime_clicked`) VALUES (NULL, '$user_id', '$item_id', '$now');";
