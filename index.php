fa6f12e01d130314787abbfd92457b4f5dfae9e7
mgwee <42485141+mgwee@users.noreply.github.com>
2018-09-07 23:43:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
                     <div class="item-carousel item">
                         <div class="carousel-des">
                             <h2>Dorcas Stitch</h2>
-                            <!--<h4>#</h4>-->
+                            <!--<h4>#1</h4>-->
                             <form method="post" action="products.php">
                                 <input type="hidden" name="merchant" value="DORCAS STITCH">
                                 <button class="block-btn carousel-btn">shop now <i class="fas fa-chevron-right"></i></button>
@@ -60,7 +60,7 @@
                     <div class="item-carousel item">
                         <div class="carousel-des">  <!-- "carousel-des-black" for black heading -->
                             <h2>H2 Hub</h2>
-                            <!--<h4>#</h4>-->
+                            <!--<h4>#2</h4>-->
                             <form method="post" action="products.php">
                                 <input type="hidden" name="merchant" value="H2 HUB">
                                 <button class="block-btn carousel-btn">shop now <i class="fas fa-chevron-right"></i></button> <!-- "carousel-btn-black" for black btn -->
@@ -73,7 +73,7 @@
                     <div class="item-carousel item">
                         <div class="carousel-des">
                             <h2>51Label</h2>
-                            <!--<h4>#</h4>-->
+                            <!--<h4>#3</h4>-->
                             <form method="post" action="products.php">
                                 <input type="hidden" name="merchant" value="51 LABEL">
                                 <button class="block-btn carousel-btn">shop now <i class="fas fa-chevron-right"></i></button>
@@ -82,17 +82,27 @@
                         <img src="img/image3.png">
                     </div>
                     
-                      <div class="item-carousel item">
+                      <!--<div class="item-carousel item">
                         <div class="carousel-des">
                             <h2>Test</h2>
-                            <!--<h4>#</h4>-->
+                            <!--<h4>#4</h4>
                             <form method="post" action="products.php">
-                                <!--<input type="hidden" name="merchant" value="51 LABEL">-->
                                 <button class="block-btn carousel-btn"><a href="https://www.lucxury.com/Website2/products.php">shop now <i class="fas fa-chevron-right"></i></a></button>
                             </form>
                         </div>
                         <img src="img/image3.png">
-                    </div>
+                    </div>-->
+                    
+                    <!--<div class="item-carousel item">
+                        <div class="carousel-des">
+                            <h2>Test</h2>
+                            <!--<h4>#5</h4>
+                            <form method="post" action="products.php">
+                                <button class="block-btn carousel-btn"><a href="https://www.lucxury.com/Website2/products.php">shop now <i class="fas fa-chevron-right"></i></a></button>
+                            </form>
+                        </div>
+                        <img src="img/image3.png">
+                    </div>-->
                 </div>
             </div>
 
