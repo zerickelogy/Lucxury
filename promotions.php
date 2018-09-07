32299f2b717c6ea25481e82bf82740dee22683d9
mgwee <42485141+mgwee@users.noreply.github.com>
2018-09-07 22:25:45 +0800

    Update promotions.php

--- promotions.php
+++ promotions.php
@@ -239,7 +239,7 @@
                     <div class="modal-header">
                         <span class="close" id="close5">&times;</span>
 
-                        <h3>Foundation Jewellers</h3><br><br>
+                        <h3>Foundation Jewellers</h3>
                         <h4>Scan the QR Code below:</h4>
                     </div>
                     <div class="modal-body">
