48bdf859259acaef19ceeb78d531101e35ceeb5c
mgwee <42485141+mgwee@users.noreply.github.com>
2018-09-07 23:31:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -82,13 +82,16 @@
                         <img src="img/image3.png">
                     </div>
                     
-                     <div class="item-carousel item">
+                      <div class="item-carousel item">
                         <div class="carousel-des">
-                             <h2>Test</h2>
-                             <!--<h4>#</h4>-->
-                           <button class="block-btn carousel-btn"><a href="https://www.lucxury.com/Website2/products.php">shop now <i class="fas fa-chevron-right"></i></a></button>
+                            <h2>Test</h2>
+                            <!--<h4>#</h4>-->
+                            <form method="post" action="products.php">
+                                <!--<input type="hidden" name="merchant" value="51 LABEL">-->
+                                <button class="block-btn carousel-btn"><a href="https://www.lucxury.com/Website2/products.php">shop now <i class="fas fa-chevron-right"></i></a></button>
+                            </form>
                         </div>
-                    <img src="img/image3.png">
+                        <img src="img/image3.png">
                     </div>
                 </div>
             </div>
