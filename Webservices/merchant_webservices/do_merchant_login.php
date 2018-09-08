4e75efa0311aae8088b64dcc8762cb46423858c8
glenn <glenn@192.168.1.15>
2018-09-08 14:43:55 +0800

    Merge origin/master
    Conflicts:
    	index.php
    	login.php

--- Webservices/merchant_webservices/do_merchant_login.php
+++ Webservices/merchant_webservices/do_merchant_login.php
@@ -14,9 +14,9 @@ if (mysqli_num_rows($result) > 0) {
     $_SESSION["merchant_name"] = $row['merchant_name'];
     $_SESSION["user_type"] = 'merchant';
 
-    $msg = "<img src='../images/WebsiteStatusImages/loadingImage.jpg'>";
+    $msg = "<img src='../../img/WebsiteStatusImages/loadingImage.jpg'>";
 } else {
-    $msg = "<img src='../images/WebsiteStatusImages/wrongUserOrPass.jpg'>";
+    $msg = "../../img/WebsiteStatusImages/wrongUserOrPass.jpg'>";
 }
 ?>
 <html>
