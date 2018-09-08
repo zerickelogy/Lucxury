e760884b69874e4087499b1dd67725f90c2f1efe
mgwee <42485141+mgwee@users.noreply.github.com>
2018-09-08 23:48:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -95,9 +95,9 @@
                     </div>
                     
                       <div class="item-carousel item">
-                        <div class="carousel-sm">
-                            <h2>Check out our exclusive promotional tie-up deals with Singapore luxury labels</h2>
-                            <!--<h4>#</h4>-->
+                        <div class="carousel-des">
+                            <h2>Check out</h2>
+                            <h4>our exclusive promotional tie-up deals with Singapore luxury labels</h4>
                             <form method="post" action="products.php">
                                 <button class="block-btn carousel-btn">shop now <i class="fas fa-chevron-right"></i></button>
                             </form>
