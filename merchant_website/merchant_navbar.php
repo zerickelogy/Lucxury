b2831710b633c8226e6ab4ca91dbe201c0d3f910
glenn <glenn@192.168.1.15>
2018-09-09 15:38:01 +0800

    Merge origin/master
    Conflicts:
    	index.php
    	login.php

--- merchant_website/merchant_navbar.php
+++ merchant_website/merchant_navbar.php
@@ -11,20 +11,27 @@ include '../../scripts/bootstrap_scripts/bootstrap_scripts.php';
             $(document).ready(function () {
                 $('#logout').click(function () {
                     if (confirm("Are you sure to logout?")) {
-                        window.location.replace("../Webservices/merchant_webservices/doLogout.php");
+                        $.ajax({
+                            type: "GET",
+                            url: "../Webservices/doLogout.php",
+                            cache: false,
+//                        dataType: "JSON",
+                            success: function (response) {
+                                console.log("result for deletion: " + response);
+                                location.reload();
+                            },
+                            error: function (obj, textStatus, errorThrown) {
+                                console.log("Error " + textStatus + ": " + errorThrown);
+                            }
+                        });
                     }
                 });
             });
-
-            function logout() {
-
-            }
-
         </script>
     </head>
     <body>
         <nav class="navbar navbar-expand-sm navbar-light bg-light">
-            <a class="navbar-brand" href="#" id="">Welcome, <?php echo $_SESSION['merchant_name'] ?></a>
+            <a class="navbar-brand" href="#">Welcome, <?php echo $_SESSION['merchant_name'] ?></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
